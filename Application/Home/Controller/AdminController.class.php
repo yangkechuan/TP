045a64646a71df3b7e9bd31e7087f67f41fa6610
ykc <907148778@qq.com>
2016-04-22 14:59:03 +0800

    增加public模板，修改配置放在Common/Conf/文件下

--- Application/Home/Controller/AdminController.class.php
+++ Application/Home/Controller/AdminController.class.php
@@ -44,7 +44,7 @@ class AdminController extends Controller{
     public function adminList(){
         $admin  = D('Admin');
         $list = $admin->adminList();
-        $this->assign('list',json_encode($list));
+        $this->assign('list',$list);
         $this->display();
     }
 }
--- index.php
+++ index.php
@@ -17,6 +17,9 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
 
+//定义使用的配置,在Common/conf/test
+define('APP_STATUS','test');
+
 // 定义应用目录
 define('APP_PATH','./Application/');
 
